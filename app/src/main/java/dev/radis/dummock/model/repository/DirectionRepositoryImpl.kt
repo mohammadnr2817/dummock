bb6438a6ddec09fe5fbc1af1a414d318f5e616f9
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-02-23 19:03:16 +0330

    WIP: Implement DirectionRepository

--- app/src/main/java/dev/radis/dummock/model/repository/DirectionRepositoryImpl.kt
+++ app/src/main/java/dev/radis/dummock/model/repository/DirectionRepositoryImpl.kt
@@ -3,25 +3,30 @@ package dev.radis.dummock.model.repository
 import dev.radis.dummock.model.dataSource.DirectionReader
 import dev.radis.dummock.model.entity.DirectionRequestModel
 import dev.radis.dummock.model.entity.Point
+import dev.radis.dummock.utils.constants.StringConstants.DEFAULT_TYPE
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class DirectionRepositoryImpl : DirectionRepository {
-    private val directionReader = DirectionReader()
+class DirectionRepositoryImpl @Inject constructor(
+    val directionReader: DirectionReader
+) :
+    DirectionRepository {
     override suspend fun getDirection(points: List<Point>): List<Point> {
         val res: MutableList<Point> = arrayListOf()
 
         for (i in points.indices - 1) {
             val directionRequest =
                 withContext(Dispatchers.IO) {
-                    res.addAll(
-                        directionReader.read(
-                            DirectionRequestModel(
-                                points[i],
-                                points[i + 1]
-                            )
+
+                    directionReader.read(
+                        DirectionRequestModel(
+                            DEFAULT_TYPE,
+                            points[i],
+                            points[i + 1]
                         )
-                    )
+                    ).ifSuccessful { }
+
                 }
         }
         return res
