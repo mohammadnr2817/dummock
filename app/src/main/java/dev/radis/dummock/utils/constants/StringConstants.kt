e5f64142b84d888ac150d2d708354b22f4a1d419
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-03-16 20:53:58 +0330

    Add bike type constants

--- app/src/main/java/dev/radis/dummock/utils/constants/StringConstants.kt
+++ app/src/main/java/dev/radis/dummock/utils/constants/StringConstants.kt
@@ -2,5 +2,8 @@ package dev.radis.dummock.utils.constants
 
 object StringConstants {
     const val BASE_DIRECTION_URL = "https://api.neshan.org/"
-    const val DEFAULT_DIRECTION_TYPE = "car"
+    const val DIRECTION_TYPE_CAR = "car"
+    const val DIRECTION_TYPE_WALK = "walk"
+    const val DIRECTION_TYPE_BIKE = "motorcycle"
+    const val DUMMOCK_TAG = "DUMMOCK_TAG"
 }
