b241355a6526c3520ed52473c95cb104d7b27750
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-03-16 21:05:23 +0330

    Add neshan api key

--- app/src/main/java/dev/radis/dummock/model/api/NeshanApiService.kt
+++ app/src/main/java/dev/radis/dummock/model/api/NeshanApiService.kt
@@ -1,11 +1,14 @@
 package dev.radis.dummock.model.api
 
+import dev.radis.dummock.BuildConfig
 import dev.radis.dummock.model.entity.response.DirectionResponseModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface NeshanApiService {
+    @Headers(BuildConfig.NESHAN_API_KEY)
     @GET("v3/direction")
     suspend fun getDirection(
         @Query("type") type: String,
