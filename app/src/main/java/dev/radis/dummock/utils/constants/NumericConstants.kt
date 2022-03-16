d76952f9115595e5a1b639040713c038c7b15083
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-03-16 20:54:28 +0330

    Add numerical constants

---
+++ app/src/main/java/dev/radis/dummock/utils/constants/NumericConstants.kt
@@ -0,0 +1,7 @@
+package dev.radis.dummock.utils.constants
+
+object NumericConstants {
+    const val FIRST_LOCATION_INDEX = 0
+    const val SECOND_LOCATION_INDEX = 1
+    const val POLYLINE_WIDTH = 4.0f
+}
