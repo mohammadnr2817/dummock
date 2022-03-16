77e6a27633f9b265da246fa48218295c332765a6
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-03-16 23:09:49 +0330

    Add default speed to map state

--- app/src/main/java/dev/radis/dummock/utils/constants/NumericConstants.kt
+++ app/src/main/java/dev/radis/dummock/utils/constants/NumericConstants.kt
@@ -4,4 +4,6 @@ object NumericConstants {
     const val FIRST_LOCATION_INDEX = 0
     const val SECOND_LOCATION_INDEX = 1
     const val POLYLINE_WIDTH = 4.0f
+    const val DEFAULT_SPEED_KMH = 50
+    const val LOCATION_PROVIDING_INTERVAL = 300L
 }
--- app/src/main/java/dev/radis/dummock/view/state/MapState.kt
+++ app/src/main/java/dev/radis/dummock/view/state/MapState.kt
@@ -3,6 +3,7 @@ package dev.radis.dummock.view.state
 import dev.radis.dummock.model.entity.DirectionModel
 import dev.radis.dummock.model.entity.Point
 import dev.radis.dummock.utils.SingleUse
+import dev.radis.dummock.utils.constants.NumericConstants
 import dev.radis.dummock.utils.constants.StringConstants
 import dev.radis.dummock.utils.mvi.MviState
 
@@ -12,4 +13,5 @@ data class MapState(
     val markers: SingleUse<List<Point>> = SingleUse(emptyList()),
     val direction: SingleUse<DirectionModel>? = null,
     var directionRequestType: String = StringConstants.DIRECTION_TYPE_CAR,
+    val speed: Int = NumericConstants.DEFAULT_SPEED_KMH
 ) : MviState
