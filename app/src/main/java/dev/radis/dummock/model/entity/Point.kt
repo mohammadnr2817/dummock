de641f0de92237326ed0dea6256c4cff1207b069
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-03-16 20:56:21 +0330

    Add string formats to point

--- app/src/main/java/dev/radis/dummock/model/entity/Point.kt
+++ app/src/main/java/dev/radis/dummock/model/entity/Point.kt
@@ -1,3 +1,18 @@
 package dev.radis.dummock.model.entity
 
-data class Point(val lat: Double, val lng: Double)
+data class Point(val lat: Double? = null, val lng: Double? = null) {
+
+    fun toRequestString(): String {
+        return "$lat,$lng"
+    }
+
+    override fun toString(): String {
+        return String.format("%.2f,%.2f", lat, lng)
+    }
+
+    fun fromRawString(input: String): Point {
+        val splitString = input.split(",")
+        return Point(splitString[0].toDouble(), splitString[1].toDouble())
+    }
+
+}
