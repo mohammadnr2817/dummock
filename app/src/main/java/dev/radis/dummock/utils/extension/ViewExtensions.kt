f7aa377f1156f13f32023ad4e5e106cb95c940bf
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-04-18 14:22:43 +0430

    Implement bounce animation

--- app/src/main/java/dev/radis/dummock/utils/extension/ViewExtensions.kt
+++ app/src/main/java/dev/radis/dummock/utils/extension/ViewExtensions.kt
@@ -1,10 +1,12 @@
 package dev.radis.dummock.utils.extension
 
+import android.animation.ValueAnimator
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.util.TypedValue
 import android.view.View
+import android.view.animation.BounceInterpolator
 
 fun View.fadeVisible() {
     this.animate().alpha(1.0f)
@@ -12,6 +14,19 @@ fun View.fadeVisible() {
 
 fun View.fadeInVisible() {
     this.animate().alpha(0.0f)
+}
+
+fun View.bounceAnimation(duration: Long) {
+    val valueAnimator = ValueAnimator.ofFloat(0F, 1F)
+    valueAnimator.duration = duration
+    valueAnimator.interpolator = BounceInterpolator()
+    valueAnimator.addUpdateListener {
+        if (it.animatedFraction < 0.5)
+            this.translationY = -1 * it.animatedFraction * 2 * 24.toPx
+        else
+            this.translationY = -1 * (1 - it.animatedFraction) * 2 * 24.toPx
+    }
+    valueAnimator.start()
 }
 
 fun Bitmap.rotate(degrees: Float): Bitmap {
