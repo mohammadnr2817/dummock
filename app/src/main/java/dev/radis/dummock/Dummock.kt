99ece2b519aa7606ac6dea9661355cc5759998f5
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-05-14 17:57:35 +0430

    Force app to use light mode

--- app/src/main/java/dev/radis/dummock/Dummock.kt
+++ app/src/main/java/dev/radis/dummock/Dummock.kt
@@ -1,12 +1,14 @@
 package dev.radis.dummock
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dev.radis.dummock.di.application.AppComponentBuilder
 
 class Dummock : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         AppComponentBuilder.context = this
     }
 
