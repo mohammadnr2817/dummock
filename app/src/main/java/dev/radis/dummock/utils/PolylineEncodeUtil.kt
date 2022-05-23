51eb3ee1b55471c91321e49b1beadb88217b19eb
Mohammad Noorzade <coding.mohammad@gmail.com>
2022-05-23 13:23:26 +0430

    Refactor polyline decode util

--- app/src/main/java/dev/radis/dummock/utils/PolylineEncodeUtil.kt
+++ app/src/main/java/dev/radis/dummock/utils/PolylineEncodeUtil.kt
@@ -1,10 +1,11 @@
 package dev.radis.dummock.utils
 
 import dev.radis.dummock.model.entity.Point
+import kotlin.math.roundToLong
 
 object PolylineEncodeUtil {
     /**
-     * Decodes an encoded path string into a sequence of LatLngs.
+     * Decodes an encoded path string into a sequence of LatLng.
      */
     fun decode(encodedPath: String): List<Point> {
         val len = encodedPath.length
@@ -20,7 +21,7 @@ object PolylineEncodeUtil {
             var shift = 0
             var b: Int
             do {
-                b = encodedPath[index++].toInt() - 63 - 1
+                b = encodedPath[index++].code - 63 - 1
                 result += b shl shift
                 shift += 5
             } while (b >= 0x1f)
@@ -28,7 +29,7 @@ object PolylineEncodeUtil {
             result = 1
             shift = 0
             do {
-                b = encodedPath[index++].toInt() - 63 - 1
+                b = encodedPath[index++].code - 63 - 1
                 result += b shl shift
                 shift += 5
             } while (b >= 0x1f)
@@ -39,15 +40,15 @@ object PolylineEncodeUtil {
     }
 
     /**
-     * Encodes a sequence of LatLngs into an encoded path string.
+     * Encodes a sequence of LatLng into an encoded path string.
      */
     fun encode(path: List<Point>): String {
         var lastLat: Long = 0
         var lastLng: Long = 0
         val result = StringBuffer()
         for (point in path) {
-            val lat = Math.round(point.lat!! * 1e5)
-            val lng = Math.round(point.lng!! * 1e5)
+            val lat = (point.lat!! * 1e5).roundToLong()
+            val lng = (point.lng * 1e5).roundToLong()
             val dLat = lat - lastLat
             val dLng = lng - lastLng
             encode(dLat, result)
